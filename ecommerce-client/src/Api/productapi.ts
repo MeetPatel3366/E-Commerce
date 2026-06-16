a14e64318ccb6facf4aa5b6d6bb522c44898f049
MeetPatel3366 <meet3366patel@gmail.com>
2026-06-16 10:03:42 +0530

    feat: add product API functions for categories and product retrieval

---
+++ ecommerce-client/src/Api/productapi.ts
@@ -0,0 +1,41 @@
+import api from "./axios";
+
+export const getCategories = async () => {
+  const res = await api.get("/products/categories");
+  const data = res.data;
+
+  return data;
+};
+
+export const getProductsByCategory = async (
+  category: string,
+  limit: number,
+  skip: number,
+) => {
+  const res = await api.get(
+    `/products/category/${category}?limit=${limit}&skip=${skip}`,
+  );
+  const data = res.data;
+
+  return data;
+};
+
+export const searchProducts = async (
+  query: string,
+  limit: number,
+  skip: number,
+) => {
+  const res = await api.get(
+    `/products/search?q=${query}&limit=${limit}&skip=${skip}`,
+  );
+  const data = res.data;
+
+  return data;
+};
+
+export const getProduct = async (id: number) => {
+  const res = await api.get(`/products/${id}`);
+  const data = res.data;
+
+  return data;
+};
